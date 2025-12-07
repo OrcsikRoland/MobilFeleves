eb992a8c5fee4cc5430ce7cdb8b75e7374dea0e5
OrcsikRoland <orcsikroland21@gmail.com>
2025-12-07 13:23:01 +0100

    Add project files.

---
+++ MobilFeleves/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobilFeleves
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MobilFeleves/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MobilFeleves")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MobilFeleves.Pages")]
